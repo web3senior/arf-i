bb2d3157729485d0f28841af6b63a013f9d1955f
web3senior <atenyun@gmail.com>
2025-01-16 03:58:14 +0330

    More code

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
         return [
           {
             source: '/api/:path*',
-            destination: ':path*',
+            destination: '/:path*',
           },
         ]
       },
